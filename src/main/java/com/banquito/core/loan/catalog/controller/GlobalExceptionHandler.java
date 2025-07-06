ede88e12d4de6cd330c47af13377147691190dba
Nayeli Michelle Tipantiza Cumbal <nmtipantiza@espe.edu.ec>
2025-07-06 00:51:50 -0500

    Functional Swagger

--- src/main/java/com/banquito/core/loan/catalog/controller/GlobalExceptionHandler.java
+++ src/main/java/com/banquito/core/loan/catalog/controller/GlobalExceptionHandler.java
@@ -14,7 +14,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Slf4j
-@RestControllerAdvice
+//@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(EntityNotFoundException.class)
