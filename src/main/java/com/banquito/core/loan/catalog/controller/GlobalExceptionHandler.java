6147b68e5da25d5b52a43a54113c563872c243bb
Angelo Andres Olmedo Camacho <aaolmedo2@espe.edu.ec>
2025-07-06 23:59:11 -0500

    add swagger

--- src/main/java/com/banquito/core/loan/catalog/controller/GlobalExceptionHandler.java
+++ src/main/java/com/banquito/core/loan/catalog/controller/GlobalExceptionHandler.java
@@ -8,13 +8,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+// import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Slf4j
-@RestControllerAdvice
+// @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(EntityNotFoundException.class)
